90511aef345cbf5b77e88d5ca3527f8a518c5811
balaji kagne <128257299+balajikagne@users.noreply.github.com>
2025-07-05 21:33:08 +0530

    Update Transaction.ts dfbd

--- src/lib/models/Transaction.ts
+++ src/lib/models/Transaction.ts
@@ -1,4 +1,4 @@
-import mongoose, { Schema, model, models } from "mongoose";
+import { Schema, model, models } from "mongoose";
 
 const TransactionSchema = new Schema({
   title: { type: String, required: true },
